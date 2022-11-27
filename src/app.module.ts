75ce4176597880ee6d0242daa77c12712b636eec
Pierre <pierre.viara@outlook.com>
2022-11-27 18:11:23 +0100

    feature(config): ⚙️ add the app configuration service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppConfigurationModule } from './infrastructure/configuration/app-configuration.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [AppConfigurationModule],
   controllers: [AppController],
   providers: [AppService],
 })
